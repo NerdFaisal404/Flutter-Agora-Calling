710805e08a373d9c0a674010440bcef992d09a1b
Faisal Ahmed <faisal.ahmedbcse@gmail.com>
2020-11-03 16:29:10 +0600

    init commit

---
+++ android/app/src/main/kotlin/app/com/flutter_agora_call/MainActivity.kt
@@ -0,0 +1,6 @@
+package app.com.flutter_agora_call
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/utils/settings.dart
@@ -0,0 +1,3 @@
+/// Define App ID and Token
+const APP_ID = "f08161480f8147acb2b44b0eaadc3f32";
+const Token = "006f08161480f8147acb2b44b0eaadc3f32IADMGv2uu9usZ9+LgtXgfBMVeqPj37hJHCPnwXOGSxs9lxoOU5QAAAAAEADnMOtSWYCiXwEAAQBXgKJf";
